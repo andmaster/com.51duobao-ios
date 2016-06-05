ffc642df25e8f53ce20d1e7dc3da230fd0a66318
QuestZhang <zs_new@126.com>
2016-06-05 22:56:38 +0800

    Initial Commit

---
+++ shop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shop
+//
+//  Created by zhangwenqiang on 16/6/5.
+//  Copyright © 2016年 ishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shop
+//
+//  Created by zhangwenqiang on 16/6/5.
+//  Copyright © 2016年 ishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
